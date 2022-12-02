53a0ab8294ee88a731920271c5a247866d7ae1a1
Shannon <shannon@Shannons-MacBook-Pro.local>
2022-12-02 06:15:50 -0600

    Trouble adding files to stage, using new command git commit -a

--- index.js
+++
@@ -1,4 +0,0 @@
-import { readFileSync } from 'fs';
-const readFile = (path) => readFileSync(path, { encoding: 'utf8' });
-const input = readFile('./inputs/day1_1.txt').split('\n');
-console.log(input);
