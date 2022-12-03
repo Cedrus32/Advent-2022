14386411f15e2cc7b72b410103534a1f9dfc5bb4
Shannon <shannon@Shannons-MacBook-Pro.local>
2022-12-03 14:43:52 -0600

    day 2, part 2 solution

--- day2/index.js
+++ day2/index.js
@@ -3,49 +3,42 @@ const readFile = (path) => readFileSync(path, { encoding: 'utf8' });
 const input = readFile('./input.txt').split('\n');
 console.log(input);
 
-// ['A Y', 'B X', 'C Z']
-// A, X === rock
-// B, Y === paper
-// C, Z === scissors
-
-// total === handshape + outcome
-//          rock === 1
-//          paper === 2
-//          scissors === 3
-//                      win === 6
-//                      tie === 3
-//                      lose === 0
-
 function getScores(array) {
     let scores = [];
     for (let i in array) {
-        let subScores = array[i].split(' ');
-        let shapeScore = getShapeScore(subScores[1]);
-        let outcomeScore = getOutcomeScore(`${subScores[0]} ${subScores[1]}`);
+        let round = array[i].split(' ');
+        let shape = getShape(`${round[0]} ${round[1]}`);
+        let shapeScore = getShapeScore(shape);
+        let outcomeScore = getOutcomeScore(round[1]);
         let roundScore = shapeScore + outcomeScore;
         scores.push(roundScore);
     }
     return scores;
 }
 
+function getShape(round) {
+    let rounds = {rock: ['A Y', 'B X', 'C Z'],
+                  paper: ['A Z', 'B Y', 'C X'],
+                  scissors: ['A X', 'B Z', 'C Y']
+                 };
+    let shape;
+    if (rounds['rock'].includes(round)) {
+        shape = 'rock';
+    } else if (rounds['paper'].includes(round)) {
+        shape = 'paper';
+    } else {
+        shape = 'scissors';
+    }
+    return shape;
+}
+
 function getShapeScore(shape) {
-    let points = {X: 1, Y: 2, Z: 3};
+    let points = {rock: 1, paper: 2, scissors: 3};
     return points[shape];
 }
 
-function getOutcomeScore(moves) {
-    let wins = ['C X', 'A Y', 'B Z'];
-    let ties = ['A X', 'B Y', 'C Z'];
-    // let losses = ['B X', 'C Y', 'A Z'];
-    let points = {win: 6, tie: 3, lose: 0};
-    let outcome;
-    if (wins.includes(moves)) {
-        outcome = 'win';
-    } else if (ties.includes(moves)) {
-        outcome = 'tie';
-    } else {
-        outcome = 'lose';
-    }
+function getOutcomeScore(outcome) {
+    let points = {Z: 6, Y: 3, X: 0};
     return points[outcome];
 }
 
@@ -60,7 +53,5 @@ function sumScores(array) {
 }
 
 let scores = getScores(input);
-// console.log(scores);
-
 let scoreTotal = sumScores(scores);
 console.log(scoreTotal);
